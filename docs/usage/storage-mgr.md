d978128959f311811acd00d6868dd5457c8aed5e
VirtualHotBar <96966978+VirtualHotBar@users.noreply.github.com>
2024-05-03 10:04:21 +0800

    Initial commit

---
+++ docs/usage/storage-mgr.md
@@ -0,0 +1,5 @@
+---
+sidebar_position: 1
+---
+
+# 存储管理
---
+++ docusaurus.config.js
@@ -0,0 +1,119 @@
+const lightCodeTheme = require('prism-react-renderer/themes/github');
+const darkCodeTheme = require('prism-react-renderer/themes/dracula');
+
+// With JSDoc @type annotations, IDEs can provide config autocompletion
+/** @type {import('@docusaurus/types').DocusaurusConfig} */
+(module.exports = {
+  title: 'NetMount Docs',
+  tagline: '统一管理和挂载云存储设施',
+  url: 'https://docs.netmount.cn',
+  baseUrl: '/',
+  onBrokenLinks: 'throw',
+  onBrokenMarkdownLinks: 'warn',
+  favicon: 'img/favicon.ico',
+  organizationName: 'VirtualHotBar', // Usually your GitHub org/user name.
+  projectName: 'NetMount', // Usually your repo name.
+
+  presets: [
+    [
+      '@docusaurus/preset-classic',
+      /** @type {import('@docusaurus/preset-classic').Options} */
+      ({
+        docs: {
+          sidebarPath: require.resolve('./sidebars.js'),
+          // Please change this to your repo.
+          editUrl: 'https://github.com/VirtualHotBar/NetMount-Docs/edit/main/',
+        },
+/*         blog: {
+          showReadingTime: true,
+          // Please change this to your repo.
+          editUrl:
+            'https://github.com/facebook/docusaurus/edit/main/website/blog/',
+        }, */
+        theme: {
+          customCss: require.resolve('./src/css/custom.css'),
+        },
+      }),
+    ],
+  ],
+
+  themeConfig:
+    /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
+    ({
+      navbar: {
+        title: 'NetMount Docs',
+        logo: {
+          alt: 'NetMount Logo',
+          src: 'img/logo.svg',
+        },
+        items: [
+/*           {
+            type: 'doc',
+            docId: 'intro',
+            position: 'left',
+            label: 'Tutorial',
+          }, */
+         /*  {to: '/blog', label: 'Blog', position: 'left'}, */
+         {
+          href: 'https://www.netmount.cn/',
+          label: '首页',
+          position: 'right',
+        },
+          {
+            href: 'https://github.com/VirtualHotBar/NetMount/',
+            label: 'GitHub',
+            position: 'right',
+          },
+        ],
+      },
+      footer: {
+        style: 'light',
+/*         links: [
+          {
+            title: 'Docs',
+            items: [
+              {
+                label: 'Tutorial',
+                to: '/docs/intro',
+              },
+            ],
+          },
+          {
+            title: 'Community',
+            items: [
+              {
+                label: 'Stack Overflow',
+                href: 'https://stackoverflow.com/questions/tagged/docusaurus',
+              },
+              {
+                label: 'Discord',
+                href: 'https://discordapp.com/invite/docusaurus',
+              },
+              {
+                label: 'Twitter',
+                href: 'https://twitter.com/docusaurus',
+              },
+            ],
+          },
+          {
+            title: 'More',
+            items: [
+              {
+                label: 'Blog',
+                to: '/blog',
+              },
+              {
+                label: 'GitHub',
+                href: 'https://github.com/facebook/docusaurus',
+              },
+            ],
+          },
+        ], */
+        copyright: `Copyright © ${new Date().getFullYear()} VirtualHotBar. `,
+      },
+      prism: {
+        theme: lightCodeTheme,
+        darkTheme: darkCodeTheme,
+      },
+    }),
+});
