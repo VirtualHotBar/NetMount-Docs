be951bece74102396062c656b84cdb18e4484c20
VirtualHotBar <96966978+VirtualHotBar@users.noreply.github.com>
2024-06-21 16:36:25 +0800

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -10,7 +10,7 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
   baseUrl: '/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
-  favicon: 'img/favicon.ico',
+  favicon: 'favicon.ico',
   organizationName: 'VirtualHotBar', // Usually your GitHub org/user name.
   projectName: 'NetMount', // Usually your repo name.
   headTags: [
