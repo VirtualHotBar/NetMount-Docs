94fe4c2b20941bdec82055868cb4ab38d35fbe8e
VirtualHotBar <96966978+VirtualHotBar@users.noreply.github.com>
2025-02-04 17:57:44 +0800

    Update main.js

--- static/js/main.js
+++ static/js/main.js
@@ -1,3 +1,11 @@
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?993ce862f60eb462df5e92616c5eaabe";
+  var s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
+})();
+
 (function(c,l,a,r,i,t,y){
         c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
         t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
